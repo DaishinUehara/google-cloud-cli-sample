3806a01aa891295463d1713266fc356bfe4207d1
DaishinUehara <git.uehara@gmail.com>
2018-08-11 23:03:40 +0900

    set zone param

--- 03_delete_all_instance.sh
+++ 03_delete_all_instance.sh
@@ -9,16 +9,19 @@ error(){
 }
 
 instances=$(gcloud compute instances list | tail -n +2 | sed -e 's/\ \ */\t/g' | cut -f 1 | tr '\n' ' ')
+
+gcloud compute instances list | \
+tail -n +2 | \
+awk '
+{
+  cmd = sprintf("gcloud compute instances delete %s --zone=%s --quiet\n",$1, $2);
+  buf = system(cmd);
+  close(cmd);
+}
+'
 status1=$?
 if [ $status1 -ne 0 ]; then
   echo "Get Instances Error!!" >&2
-  eixt $status1
-fi
-
-gcloud compute instances delete ${instances} --quiet
-status1=$?
-if [ $status1 -ne 0 ]; then
-  echo "Delete Instances Error!!" >&2
   eixt $status1
 fi
 
